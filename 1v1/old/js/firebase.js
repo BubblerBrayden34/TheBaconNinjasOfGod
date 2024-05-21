d2b7680312b3b5b5da3e53ae86e599eda932fbea
ComputerGuy11 <coltcomicsinc7667@gmail.com>
2024-05-20 20:12:43 -0400

    Update firebase.js

--- 1v1/old/js/firebase.js
+++ 1v1/old/js/firebase.js
@@ -1,8 +1,8 @@
 function initializeFireBase(){
 	// Your web app's Firebase configuration
 	var firebaseConfig = {
-	apiKey: "AIzaSyBPrAfspM9RFxuNuDtSyaOZ5YRjDBNiq5I",
-	authDomain: "1v1.lol",
+	apiKey: "",
+	authDomain: "1v1.lol/neocities",
 	databaseURL: "https://justbuild-cdb86.firebaseio.com",
 	projectId: "justbuild-cdb86",
 	storageBucket: "justbuild-cdb86.appspot.com",
@@ -16,7 +16,7 @@ function initializeFireBase(){
 function initializeFireBaseDev(){
 	// Your web app's Firebase configuration
 	var firebaseConfig = {
-	apiKey: "AIzaSyANZ0SDhqoc62msSooQFs3SEb4XbC7gvk4",
+	apiKey: ",
 	authDomain: "dev.1v1.lol",
 	databaseURL: "https://dev1v1.firebaseio.com",
 	projectId: "dev1v1",
